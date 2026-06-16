d4b536a1ea933bf202db18bad86740e904b78922
JuliaNedilchak <115113067+JuliaNedilchak@users.noreply.github.com>
2026-06-16 22:56:48 +0100

    header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import AuthNav from './AuthNav'
 import UserNav from './UserNav';
+import css from './Header.module.css'
 
 const Header = () => {
   const isLoggedIn=false;
   return (
-    <header>
-        <Link to='/home'>petlove</Link>
-        <nav>
-            <Link to='/news'>News</Link>
-            <Link to= '/notices'>Find pet</Link>
-            <Link to='/friends'>Our friends</Link>
+    <header className={css.header}>
+        <Link className={css.symbol} to='/home'>petlove</Link>
+        <nav className={css.nav}>
+            <Link className={css.category} to='/news'>News</Link>
+            <Link className={css.category} to= '/notices'>Find pet</Link>
+            <Link className={css.category} to='/friends'>Our friends</Link>
         </nav>
        {isLoggedIn ? <UserNav/> : <AuthNav/>}
     </header>
--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import hero from '../../assets/hero.jpg';
 import css from './Hero.module.css';
 import '../../styles/global.css';
+import Header from '../../components/Header/Header';
 
 const Hero = () => {
   return (
     <section className={css.hero}>
-    <div className={css.container}>
+    <div className='container'>
     <div className={css.upperBlock}>
-      <h1 >Take good care of your small pets</h1>
-      <p>Choosing a pet for your home is a choice that is meant to enrich your life with immeasurable joy and tenderness./</p>
+    <Header />
+    <div className={css.heroContent}>
+      <h1 className={css.call}>Take good care of your small pets</h1>
+      <p className={css.heroText}>Choosing a pet for your home is a choice that is meant to enrich your life with immeasurable joy and tenderness.</p>
+     </div>
       </div>
       <div className={css.heroImageWrapper}>
       <img className={css.heroImage} src={hero} alt='someone with a dog'/></div>
