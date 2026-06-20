92dc9d92f448c7a24f2361f03ff1d8f772bd5625
JuliaNedilchak <115113067+JuliaNedilchak@users.noreply.github.com>
2026-06-20 23:14:45 +0100

    friends page

--- frontend/src/components/FriendCard/FriendCard.jsx
+++ frontend/src/components/FriendCard/FriendCard.jsx
@@ -1,9 +1,10 @@
-
+import css from './FriendCard.module.css';
 
 const FriendCard = ({friend}) => {
   return (
-    <li>
-      <img src={friend.imageUrl} alt={friend.title}/>
+    <li className={css.card}>
+    <span className={css.workTime}>Day and night</span>
+      <img className={css.logo} src={friend.imageUrl} alt={friend.title}/>
       <h2>{friend.title}</h2>
       <p>Email: {friend.email}</p>
       <p>Address: {friend.address}</p>
--- frontend/src/components/FriendsList/FriendsList.jsx
+++ frontend/src/components/FriendsList/FriendsList.jsx
@@ -1,9 +1,10 @@
 
 import FriendCard from '../FriendCard/FriendCard'
+import css from './FriendsList.module.css'
 
 const FriendsList = ({friends}) => {
   return (
-    <ul>
+    <ul className={css.friendsList}>
       {friends.map(friend =>(
         <FriendCard key={friend._id}
         friend={friend}/>
