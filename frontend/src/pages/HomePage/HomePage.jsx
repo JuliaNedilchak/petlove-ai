f4b28a89039f0070181c84120580799f3b32d3b3
JuliaNedilchak <115113067+JuliaNedilchak@users.noreply.github.com>
2026-06-30 21:55:48 +0100

    register

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import Hero from '../../components/Hero/Hero'
-import { useDispatch, useSelector }  from 'react-redux';
-import { setUser } from '../../redux/auth/authSlice';
 
 
 const HomePage = () => {
--- frontend/src/pages/RegisterPage/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,20 +1,54 @@
-import React from 'react'
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+
+import { useDispatch } from 'react-redux';
 import * as Yup from 'yup';
+import { register } from '../../redux/auth/authOperations';
+
+const initialValues=({
+  name:'',
+  email:'',
+  password:'',
+})
 
 const RegisterSchema= Yup.object({
   name:
   Yup.string().required(),
   email:
-  Yup.email('Invalid email').required('email is required'),
+  Yup.string().email('Invalid email').required('email is required'),
   password:
   Yup.string().min(7, 'Password must be at least 7 characters').required('password is required')
 })
 
 const RegisterPage = () => {
-  return (
-    <div>
-      register
+  const dispatch= useDispatch();
+ const handleSubmit=(values)=>{
+  dispatch(register(values))};
+    return (
+   <section>
+    <div className='container'>
+      <Formik
+      initialValues={initialValues}
+       validationSchema={RegisterSchema} 
+       onSubmit={handleSubmit}>
+       <Form>
+        <label>
+          name
+<Field name='name' type='name' placeholder='name'/>
+<ErrorMessage name='name' component='p'/>
+        </label>
+        <label>
+          <Field name='email' type='email' placeholder='email'/>
+          <ErrorMessage name='email' component='p'/>
+          </label>
+           <label>
+          <Field name='password' type='password' placeholder='password'/>
+          <ErrorMessage name='password' component='p'/>
+          </label>
+          <button type='submit'>Registration</button>
+       </Form>
+</Formik>
     </div>
+   </section>
   )
 }
 
